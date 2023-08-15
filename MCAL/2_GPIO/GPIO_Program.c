12d94fbe461d77c12926ef30bc29b8245bd6f936
Kareem Walid <engkareemwalid1@gmail.com>
2023-08-15 09:16:22 -0700

    Update gpio.c driver and solve some buges

--- MCAL/2_GPIO/GPIO_Program.c
+++ MCAL/2_GPIO/GPIO_Program.c
@@ -9,20 +9,25 @@ void GPIO_PinConfig(SPinConfig_t *SPinConfigPoin)
 	switch(SPinConfigPoin->Port)
 	{
 	case GPIO_PA:
+
 		// Mode
 		GPIOA -> GPIO_MODER   &=~ (0b11<<((SPinConfigPoin->Pin)*2));
 		GPIOA -> GPIO_MODER   |=  (((SPinConfigPoin->PinMode)<<(SPinConfigPoin->Pin)*2));
 
 		// LEVEL   // TODO USE GPIOA_BSRR
-		if((GPIOA -> GPIO_MODER) == OUTPUT_M )
+
+
+		if(GET_BIT((GPIOA -> GPIO_MODER),(SPinConfigPoin->Pin)*2) == OUTPUT_M )
 		{
 			if((SPinConfigPoin->Level) == PIN_HIGH)
 				SET_BIT(GPIOA -> GPIO_ODR,SPinConfigPoin->Pin);
 			else
 				CLR_BIT(GPIOA -> GPIO_ODR,SPinConfigPoin->Pin);
-		}else if ((GPIOA -> GPIO_MODER) == INPUT_M )
+
+		}
+		else if (GET_BIT((GPIOA -> GPIO_MODER),(SPinConfigPoin->Pin)*2) == INPUT_M )
 		{
-			SPinConfigPoin->Level = GPIOA->GPIO_IDR;
+			SPinConfigPoin-> Level = GET_BIT(GPIOA->GPIO_IDR ,SPinConfigPoin->Pin);
 		}
 
 		// Speed
