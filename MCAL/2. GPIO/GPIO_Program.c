42d473d4dac421076d5f27a1af951b1cb687c96e
Kareem Walid <engkareemwalid1@gmail.com>
2023-09-01 23:47:02 +0300

    driver gpio

--- MCAL/2. GPIO/GPIO_Program.c
+++ MCAL/2. GPIO/GPIO_Program.c
@@ -1,142 +1,198 @@
+/**********************************************************************************************************************
+ *  FILE INFORMATION
+ *  -----------------------------------------------------------------------------------------------------------------
+ *        FILE NAME :  GPIO_Program.c
+ *        FILE DATE :  10/8/2023
+ *        VERSION:   V2.0
+ *        AUTHOR:   KAREEM WALID
+ *        BRIEF EXPLANATION:
+ *
+ **********************************************************************************************************************
+
+   ==============================================================================
+                    ##### GPIO Peripheral Features #####
+   ==============================================================================
 
 
+
+  ==============================================================================
+                     ##### How To Use This Driver #####
+  ==============================================================================
+
+
+
+
+/* *********************************************************************************************************************
+ *  INCLUDES
+ *********************************************************************************************************************/
 #include "GPIO_Interface.h"
 
-void GPIO_PinConfig(SPinConfig_t *SPinConfigPoin)
+
+/* ===============================================================================
+              ##### Initialization and de-initialization functions #####
+   =============================================================================== */
+
+/******************************************************************************
+ * \Syntax          : Std_ReturnType FunctionName(AnyType parameterName)
+ * \Description     : Describe this service
+ *
+ * \Sync\Async      : Synchronous
+ * \Reentrancy      : Non Reentrant
+ * \Parameters (in) : parameterName   Parameter Describtion
+ * \Parameters (out): None
+ * \Return value:   : Std_ReturnType  E_OK
+ *                                    E_NOT_OK
+ *******************************************************************************/
+void MGPIO_vPinInit(GPIO_PORTs_t Port ,GPIO_PINs_t Pin ,SPinConfig_t *pSPinConfig)
 {
 
-
-	switch(SPinConfigPoin->Port)
+	/*Switch on Ports GPIOA , GPIOB , GPIOC , GPIOD , GPIOE or GPIOH */
+	switch(Port)
 	{
-	case GPIO_PA:
+	case GPIOA:
 
-		// Mode
-		/*
-		 *00: Input (reset state)
-		 * 01 General purpose output mode
+		/* -------------- SET MODE ------------------- */
+		/* MODE Options:
+		 * 00: Input (reset state)
+		 * 01: General purpose output mode
 		 * 10: Alternate function mode
-		 * 11: Analog mode */
-		GPIOA -> GPIO_MODER   &=~ (0b11<<((SPinConfigPoin->Pin)*2));
-		GPIOA -> GPIO_MODER   |=  (((SPinConfigPoin->PinMode)<<(SPinConfigPoin->Pin)*2));
-
-		// LEVEL   // TODO USE GPIOA_BSRR
-
-		if( SPinConfigPoin -> PinMode == OUTPUT_M )
-		{
-			if((SPinConfigPoin->Level) == PIN_HIGH)
-				SET_BIT(GPIOA -> GPIO_ODR,SPinConfigPoin->Pin);
-			else
-				CLR_BIT(GPIOA -> GPIO_ODR,SPinConfigPoin->Pin);
-
-		}else if (SPinConfigPoin -> PinMode == INPUT_M )
-		{
-			SPinConfigPoin -> Level = GET_BIT(GPIOA->GPIO_IDR ,SPinConfigPoin->Pin);
-		}
-
-		// Speed
-		/*
-		 * 00: Low speed
-         * 01: Medium speed
-         * 10: High speed
-         * 11: Very high speed
-
+		 * 11: Analog mode
 		 */
-		GPIOA -> GPIO_OSPEEDER  &=~ (0b11<<((SPinConfigPoin->Pin)*2));
-		GPIOA -> GPIO_OSPEEDER  |=  (((SPinConfigPoin->PinOutSpeed)<<(SPinConfigPoin->Pin)*2));
+
+		/* CLEAR TWO PINS SPECIFIC  */
+		CLR_TWO_BIT(pGPIOA -> GPIO_MODER,Pin);
+		/* SET TWO PINS SPECIFIC WITH MODE */
+		SET_TWO_BIT(pGPIOA -> GPIO_MODER,Pin,pSPinConfig->PinMode);
+		/* ----------------------------------------------------- */
+		/* -------------- SET PIN OUTPUT TYPE ------------------- */
+		/* OUTPUT TYPE Options:
+		 * 0: Output push-pull (reset state)
+		 * 1: Output open-drain
+		 */
 
 
-		// PinOutType Push pull / Open drain
-		CLR_BIT(GPIOA -> GPIO_OTYPER,SPinConfigPoin->Pin);
+		/* ----------------------------------------------------- */
+		/* -------------- SET OUTPUT SPEED --------------------- */
+		/* SPEED Options:
+		 * 00: Low speed
+		 * 01: Medium speed
+		 * 10: High speed
+		 * 11: Very high speed
+		 */
 
-		// PULL UP/DOWN
-		GPIOA -> GPIO_PUPDR  &=~ (0b11<<((SPinConfigPoin->Pin)*2));
-		GPIOA -> GPIO_PUPDR  |=  (((SPinConfigPoin->PinInputType)<<(SPinConfigPoin->Pin)*2));
-
-
-		/* Alternative Function Section */
-		/* Check on The Pin to  choosing register High or Low*/
-		/* If The Function Between Pin0 and Pin7 */
-		if(SPinConfigPoin->Pin >= PIN0 && SPinConfigPoin->Pin <= PIN7)
-		{
-			/* -- Save Value of AF in GPIOA_AFRL -- */
-			/* Restore bits wanted to 0000 with kepting other bits without any changes */
-			GPIOA -> GPIO_AFRL   &=~ (0b1111<<((SPinConfigPoin->Pin)*4)); 
-            /* Store 4 bits in The Reg AFRL */			
-		    GPIOA -> GPIO_AFRL   |=  (((SPinConfigPoin -> ALT_FUN)<<(SPinConfigPoin->Pin)*4));
-
-                  /* If The Function Between Pin8 and Pin15 */
-		}else if (SPinConfigPoin->Pin >= PIN7 && SPinConfigPoin->Pin <= PIN15)
-		{
-			/* -- Save Value of AF in GPIOA_AFRL -- */
-			/* Restore bits wanted to 0000 with kepting other bits without any changes */
-			GPIOA -> GPIO_AFRH   &=~ (0b1111<<((SPinConfigPoin->Pin)*4)); 
-            /* Store 4 bits in The Reg AFRH */			
-		    GPIOA -> GPIO_AFRH   |=  (((SPinConfigPoin -> ALT_FUN)<<(SPinConfigPoin->Pin)*4));
-		}
+		/* CLEAR TWO PINS SPECIFIC  */
+		CLR_TWO_BIT(pGPIOA -> GPIO_OSPEEDER,Pin);
+		/* SET TWO PINS SPECIFIC WITH SPEED */
+		SET_TWO_BIT(pGPIOA -> GPIO_OSPEEDER,Pin,pSPinConfig->PinOutSpeed);
+		/* ----------------------------------------------------- */
+		/* -------------- SET PIN INPUT TYPE ------------------- */
+		/* INPUT TYPE Options:
+		 * 00: No pull-up, pull-down
+		 * 01: Pull-up
+		 * 10: Pull-down
+		 * 11: Reserved
+		 */
+		/* CLEAR TWO PINS SPECIFIC  */
+		CLR_TWO_BIT(pGPIOA -> GPIO_PUPDR,Pin);
+		/* SET TWO PINS SPECIFIC WITH SPEED */
+		SET_TWO_BIT(pGPIOA -> GPIO_PUPDR,Pin,pSPinConfig->PinInputType);
+		/* ----------------------------------------------------- */
 		break;
 
-	case GPIO_PB:
-
-			// Mode
-			GPIOB -> GPIO_MODER   &=~ (0b11<<((SPinConfigPoin->Pin)*2));
-			GPIOB -> GPIO_MODER   |=  (((SPinConfigPoin->PinMode)<<(SPinConfigPoin->Pin)*2));
-
-			// LEVEL   // TODO USE GPIOB_BSRR
-
-			if( SPinConfigPoin -> PinMode == OUTPUT_M )
-			{
-				if((SPinConfigPoin->Level) == PIN_HIGH)
-					SET_BIT(GPIOB -> GPIO_ODR,SPinConfigPoin->Pin);
-				else
-					CLR_BIT(GPIOB -> GPIO_ODR,SPinConfigPoin->Pin);
-
-			}else if (SPinConfigPoin -> PinMode == INPUT_M )
-			{
-				SPinConfigPoin -> Level = GET_BIT(GPIOB->GPIO_IDR ,SPinConfigPoin->Pin);
-			}
-
-			// Speed
-			GPIOB -> GPIO_OSPEEDER  &=~ (0b11<<((SPinConfigPoin->Pin)*2));
-			GPIOB -> GPIO_OSPEEDER  |=  (((SPinConfigPoin->PinOutSpeed)<<(SPinConfigPoin->Pin)*2));
 
 
-			// PinOutType Push pull / Open drain
-			CLR_BIT(GPIOB -> GPIO_OTYPER,SPinConfigPoin->Pin);
+	case GPIOB:
 
-			// PULL UP/DOWN
-			GPIOB -> GPIO_PUPDR  &=~ (0b11<<((SPinConfigPoin->Pin)*2));
-			GPIOB -> GPIO_PUPDR  |=  (((SPinConfigPoin->PinInputType)<<(SPinConfigPoin->Pin)*2));
-
-
-			/* Alternative Function Section */
-			/* Check on The Pin to  choosing register High or Low*/
-			/* If The Function Between Pin0 and Pin7 */
-			if(SPinConfigPoin->Pin >= PIN0 && SPinConfigPoin->Pin <= PIN7)
-			{
-				/* -- Save Value of AF in GPIOA_AFRL -- */
-				/* Restore bits wanted to 0000 with kepting other bits without any changes */
-				GPIOB -> GPIO_AFRL   &=~ (0b1111<<((SPinConfigPoin->Pin)*4));
-	            /* Store 4 bits in The Reg AFRL */
-				GPIOB -> GPIO_AFRL   |=  (((SPinConfigPoin -> ALT_FUN)<<(SPinConfigPoin->Pin)*4));
-
-	                  /* If The Function Between Pin8 and Pin15 */
-			}else if (SPinConfigPoin->Pin >= PIN7 && SPinConfigPoin->Pin <= PIN15)
-			{
-				/* -- Save Value of AF in GPIOA_AFRL -- */
-				/* Restore bits wanted to 0000 with kepting other bits without any changes */
-				GPIOB -> GPIO_AFRH   &=~ (0b1111<<((SPinConfigPoin->Pin)*4));
-	            /* Store 4 bits in The Reg AFRH */
-				GPIOB -> GPIO_AFRH   |=  (((SPinConfigPoin -> ALT_FUN)<<(SPinConfigPoin->Pin)*4));
-			}
-			break;
-
-	default:
-        /* do nothing */
 		break;
- 	}
+
+	case GPIOC:
+
+		break;
+
+	case GPIOD:
+		break;
+
+	case GPIOE:
+		break;
+
+	case GPIOH:
+		break;
+	}
+
+}
+
+/******************************************************************************
+ * \Syntax          : Std_ReturnType FunctionName(AnyType parameterName)
+ * \Description     : Describe this service
+ *
+ * \Sync\Async      : Synchronous
+ * \Reentrancy      : Non Reentrant
+ * \Parameters (in) : parameterName   Parameter Describtion
+ * \Parameters (out): None
+ * \Return value:   : Std_ReturnType  E_OK
+ *                                    E_NOT_OK
+ *******************************************************************************/
+void MGPIO_vPinDeInit(GPIO_PORTs_t Port , SPinConfig_t *SPinConfigPoin)
+{
+
+}
+
+/* ===============================================================================
+                     ##### IO operation functions #####
+     =============================================================================== */
+
+/******************************************************************************
+ * \Syntax          : Std_ReturnType FunctionName(AnyType parameterName)
+ * \Description     : Describe this service
+ *
+ * \Sync\Async      : Synchronous
+ * \Reentrancy      : Non Reentrant
+ * \Parameters (in) : parameterName   Parameter Describtion
+ * \Parameters (out): None
+ * \Return value:   : Std_ReturnType  E_OK
+ *                                    E_NOT_OK
+ *******************************************************************************/
+void MGPIO_vWritePin(GPIO_PORTs_t Port ,GPIO_PINs_t Pin,GPIO_PIN_STATUS_t Level)
+{
 
 
 }
 
+/******************************************************************************
+ * \Syntax          : Std_ReturnType FunctionName(AnyType parameterName)
+ * \Description     : Describe this service
+ *
+ * \Sync\Async      : Synchronous
+ * \Reentrancy      : Non Reentrant
+ * \Parameters (in) : parameterName   Parameter Describtion
+ * \Parameters (out): None
+ * \Return value:   : Std_ReturnType  E_OK
+ *                                    E_NOT_OK
+ *******************************************************************************/
+void MGPIO_vTogglePin(GPIO_PORTs_t Port,GPIO_PINs_t Pin)
+{
 
+
+}
+
+/******************************************************************************
+ * \Syntax          : Std_ReturnType FunctionName(AnyType parameterName)
+ * \Description     : Describe this service
+ *
+ * \Sync\Async      : Synchronous
+ * \Reentrancy      : Non Reentrant
+ * \Parameters (in) : parameterName   Parameter Describtion
+ * \Parameters (out): None
+ * \Return value:   : Std_ReturnType  E_OK
+ *                                    E_NOT_OK
+ *******************************************************************************/
+GPIO_PINs_t  MGPIO_vReadPin(GPIO_PORTs_t Port,GPIO_PINs_t Pin)
+{
+
+	return 0;
+}
+
+/**********************************************************************************************************************
+ *  END OF FILE: GPIO_Program.c
+ *********************************************************************************************************************/
 
